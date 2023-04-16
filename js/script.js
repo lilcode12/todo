250a421a195c92e40b8ed3699cf17267c313a5e0
Vishnu Durgi <126440894+lilcode12@users.noreply.github.com>
2023-04-16 19:36:53 +0800

    edited todolistclicked

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function showActiveTasks( ) {
     }
 }
 
-function onTodoListContainerClicked(event){
+function onTodoListClicked(event){
     while (!targetElement.classList.contains("task")){
      targetElement = targetElement.parentElement;
     }
